119154153bc8dab5718b04ce4402b851f21d7234
Mirthvika <72354739+Mirthvika@users.noreply.github.com>
2020-10-04 13:15:43 -0500

    Assignment1

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main{
+
+public static void main (String[] args)
+{
+  CalculatorApp app = new CalculatorApp ();
+  app.add(1,2);
+  app.subtract(4,3);
+  app.divide(5,5);
+  app.multiply(6,6);
+}
+}
