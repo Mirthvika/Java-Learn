ba597a3ba5e9278499f6c131a126a76de8df5fcc
Mirthvika <72354739+Mirthvika@users.noreply.github.com>
2020-10-11 12:16:11 -0500

    Create CalculatorApp.java

---
+++ CalculatorApp.java
@@ -0,0 +1,18 @@
+class CalculatorApp{
+ 
+ int add(int a, int b){
+   return a + b;
+ }
+ 
+ int subtract(int a, int b){
+   return a - b;
+ }
+ 
+ double divide(int a, int b){
+   return a/b;
+ }
+ 
+ int multiply(int a, int b){
+   return a * b;
+ }
+}
