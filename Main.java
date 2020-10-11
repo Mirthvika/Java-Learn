eeaf1e0c2fad11b99fe0f56a5272ad65dc9d664e
Mirthvika <72354739+Mirthvika@users.noreply.github.com>
2020-10-11 12:16:46 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,20 @@
-class Main{
+class Main {
+ 
+ public static void main(String[] args) {
+   CalculatorApp app = new CalculatorApp ();
+   int a = app.add(1,2);
+   int b = app.subtract(4,3);
+   double c = app.divide(6,5);
+   double f = app.divide(6,6);
+   double d = app.divide(6,5);
+   int e = app.multiply(6,6);
+   System.out.println("adding 1 and 2 gives: " + a);
+   System.out.println("subtracting 4 and 3 gives: " + b);
+   System.out.println("dividing 6 and 5 gives: " + c);
+   System.out.println("dividing 6 and 5 gives: " + d);
+   System.out.println("multiplying 6 and 6 gives: " + e);
+   System.out.println("dividing 6 and 6 gives: " + f);
+ }
+ 
+}
 
-public static void main (String[] args)
-{
-  CalculatorApp app = new CalculatorApp ();
-  int a = app.add(1,2);
-  int b = app.subtract(4,3);
-  int c = app.divide(6,5);
-  double d = app.divide(6,5);
-  int e = app.multiply(6,6);
-}
-}
